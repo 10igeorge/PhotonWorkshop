988fd4ce7ec3464c7568262188373e18aa08124a
YoriKv <yori@sleepninjagames.com>
2016-06-08 16:39:49 -0700

    Prefabbed network player object

--- Assets/Scripts/Network/NetworkPlayer.cs
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class NetworkPlayer:Photon.MonoBehaviour {
     public GameObject tankPrefab;
-    private GameObject currentTank;
+    private TankController currentTank;
 
     public void Awake() {
         // If we started on the wrong scene, go back to the menu
@@ -13,6 +13,30 @@ public class NetworkPlayer:Photon.MonoBehaviour {
             return;
         }
         // Instantiate player's tank
-        currentTank = PhotonNetwork.Instantiate(tankPrefab.name, transform.position, Quaternion.identity, 0);
+        GameObject tankGO = PhotonNetwork.Instantiate(tankPrefab.name, transform.position, Quaternion.identity, 0);
+        currentTank = tankGO.GetComponent<TankController>();
+    }
+
+    public void Update() {
+        // Use escape to quit
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            // When we leave, all our objects will be automatically destroyed
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+
+    public void OnLeftRoom() {
+        // Left room? Back to main menu
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void OnDisconnectedFromPhoton() {
+        // Disconnected? Back to main menu
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void OnFailedToConnectToPhoton() {
+        // Failed to connect? Back to main menu
+        SceneManager.LoadScene("Menu");
     }
 }
