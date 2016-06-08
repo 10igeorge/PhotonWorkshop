15a434c957eb787d99a5cec5a7fc876e30482077
YoriKv <yori@sleepninjagames.com>
2016-06-08 14:51:44 -0700

    Basic menu UI and join workflow

---
+++ Assets/Scripts/UI/JoinGameMenu.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections;
+using Photon;
+using UnityEngine.UI;
+
+public class JoinGameMenu:PunBehaviour {
+    public Button joinGameButton;
+    public Image statusPanel;
+    public Text statusText;
+
+    public void Start() {
+#if UNITY_EDITOR
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+#endif
+        // Connect
+        PhotonNetwork.ConnectUsingSettings("0.1");
+        // Status
+        SetStatus("Connecting...", Color.yellow);
+        // Start with join game button disabled
+        joinGameButton.interactable = false;
+    }
+
+    // ******************** Network State ********************
+
+    public override void OnJoinedLobby() {
+        // Status
+        SetStatus("Connected to Lobby", Color.green);
+        // Enable join button
+        joinGameButton.interactable = true;
+    }
+
+    public override void OnPhotonRandomJoinFailed(object[] codeAndMsg) {
+        SetStatus("Join room failed, creating new room", Color.red);
+        // Couldn't join a room, create one for us instead
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    public override void OnJoinedRoom() {
+        SetStatus("Joined room successfully", Color.green);
+    }
+
+    // ******************** UI Interactions ********************
+
+    public void ClickJoin() {
+        Debug.Log("Clicked join!");
+        // Prevent double click
+        joinGameButton.interactable = false;
+        // Try joining a room
+        PhotonNetwork.JoinRandomRoom();
+        // Status
+        SetStatus("Joining random room", Color.yellow);
+    }
+
+    private void SetStatus(string status, Color color) {
+        statusText.text = status;
+        statusPanel.color = color;
+    }
+}
